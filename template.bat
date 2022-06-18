ad43b035ea6b044c904e4761acea5a3ec9a2197a
eliseu gili <gili.eliseup@gmail.com>
2022-06-18 20:56:26 -0300

    ajustes script e template

--- template.bat
+++ template.bat
@@ -1,6 +1,6 @@
 @echo off → evita escrever os comando na tela pela própria segurança dos usuário
-set NODE_MYSQL_SERVIDOR=a_maquina_que_roda_mysql
-set NODE_MYSQL_USUARIO=seu_usuario_no_mysql
-set NODE_MYSQL_SENHA=sua_senha_no_mysql
-set NODE_MYSQL_BD=seu_bd_no_mysql
+set NODE_MYSQL_SERVIDOR = a_maquina_que_roda_mysql
+set NODE_MYSQL_USUARIO = seu_usuario_no_mysql
+set NODE_MYSQL_SENHA = sua_senha_no_mysql
+set NODE_MYSQL_BD = seu_bd_no_mysql
 @echo Variaveis de ambiente ajustadas com sucesso!
