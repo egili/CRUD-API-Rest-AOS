ba94a1458df60daca9c2d549c2323972b12fa045
eliseu gili <gili.eliseup@gmail.com>
2022-06-26 12:49:36 -0300

    n

--- principal.js
+++ principal.js
@@ -1,4 +1,19 @@
-
 function cadastrar() {
+    let nome = document.getElementById('nome').value;
+    let tel = document.getElementById('tel').value;
+    let cpf = document.getElementById('CPF').value;
+    let cep = document.getElementById('CEP').value;
+    let numCasa = document.getElementById('NumCa').value;
+    let complemento = document.getElementById('complemento').value;
 
+    const json = {"CPF": cpf, "nome": nome, "telefone": tel, "numeroCasa": numCasa, "complemento": complemento, "CEP": cep};
+
+    axios.post('http://localhost:3000/cidadaos', json)
+        .then((e) => {
+            location.reload();
+            alert("Dados Enviados com sucesso");
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 }
